f5919b219ae047ab58f6d28dd4044b4c4ec71f51
pokrywa1 <h.pokrywka@wp.pl>
2022-11-15 21:10:54 +0100

    fetch data from hygraph cms

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -2,79 +2,53 @@ import React from 'react';
 import classes from './ArticleList.module.css';
 import Image from 'next/image';
 import Preview from '../public/img/box.png';
-const ArticleList = () => {
+const ArticleList = ({ posts }) => {
+  console.log(posts);
+
   return (
     <section className={classes.section__container}>
-      <div className={classes.article}>
-        <Image
-          src={Preview}
-          alt=""
-          width="300"
-          height="300"
-          className={classes.img}
-        />
-        <span>march 10, 2021</span>
-        <h3>Memilh foto yang untuk desain website</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis
-          reiciendis sequi magnam incidunt nobis vitae reprehenderit, ad enim
-          ipsa recusandae voluptas totam unde! A ratione corporis minus delectus
-          assumenda perspiciatis.dnadnaidnaid.
-        </p>
-      </div>
-      <div className={classes.article}>
-        <Image
-          src={Preview}
-          alt=""
-          width="300"
-          height="300"
-          className={classes.img}
-        />
-        <span>march 10, 2021</span>
-        <h3>Memilh foto yang untuk desain website</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis
-          reiciendis sequi magnam incidunt nobis vitae reprehenderit, ad enim
-          ipsa recusandae voluptas totam unde! A ratione corporis minus delectus
-          assumenda perspiciatis.
-        </p>
-      </div>
-      <div className={classes.article}>
-        <Image
-          src={Preview}
-          alt=""
-          width="300"
-          height="300"
-          className={classes.img}
-        />
-        <span>march 10, 2021</span>
-        <h3>Memilh foto yang untuk desain website</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis
-          reiciendis sequi magnam incidunt nobis vitae reprehenderit, ad enim
-          ipsa recusandae voluptas totam unde! A ratione corporis minus delectus
-          assumenda perspiciatis.
-        </p>
-      </div>
-      <div className={classes.article}>
-        <Image
-          src={Preview}
-          alt=""
-          width="300"
-          height="300"
-          className={classes.img}
-        />
-        <span>march 10, 2021</span>
-        <h3>Memilh foto yang untuk desain website</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis
-          reiciendis sequi magnam incidunt nobis vitae reprehenderit, ad enim
-          ipsa recusandae voluptas totam unde! A ratione corporis minus delectus
-          assumenda perspiciatis.
-        </p>
-      </div>
+      {posts.map(post => {
+        const text = post.content.text.slice(0, 150) + '...';
+        const src = post.coverPhoto.url;
+        return (
+          <div className={classes.article}>
+            <Image
+              src={src}
+              alt=""
+              width="300"
+              height="300"
+              className={classes.img}
+            />
+            <span>{post.datePublished}</span>
+            <h3>{post.title}</h3>
+            <p>{text}</p>
+          </div>
+        );
+      })}
     </section>
   );
+
+  // return (
+  //   <section className={classes.section__container}>
+  //     <div className={classes.article}>
+  //       <Image
+  //         src={Preview}
+  //         alt=""
+  //         width="300"
+  //         height="300"
+  //         className={classes.img}
+  //       />
+  //       <span>march 10, 2021</span>
+  //       <h3>Memilh foto yang untuk desain website</h3>
+  //       <p>
+  //         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis
+  //         reiciendis sequi magnam incidunt nobis vitae reprehenderit, ad enim
+  //         ipsa recusandae voluptas totam unde! A ratione corporis minus delectus
+  //         assumenda perspiciatis.dnadnaidnaid.
+  //       </p>
+  //     </div>
+  //   </section>
+  // );
 };
 
 export default ArticleList;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,47 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import ArticleList from '../components/ArticleList';
 import LatestArticle from '../components/LatestArticle';
 import styles from '../styles/Home.module.css';
+import { GraphQLClient, gql } from 'graphql-request';
 
-export default function Home() {
+const graphcms = new GraphQLClient(
+  'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/claikqze01wbx01tc3m3leyy5/master'
+);
+
+const QUERY = gql`
+  query {
+    posts {
+      id
+      title
+      datePublished
+      slug
+      content {
+        text
+      }
+      coverPhoto {
+        url
+      }
+    }
+  }
+`;
+
+export async function getStaticProps() {
+  const { posts } = await graphcms.request(QUERY);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+export default function Home({ posts }) {
+  console.log(posts);
   return (
     <Fragment>
       <LatestArticle></LatestArticle>
-      <ArticleList></ArticleList>
+      <ArticleList posts={posts}></ArticleList>
     </Fragment>
   );
 }
